9e5fbb487ab7d2a888306119ab2253d5a3493d8d
Loïc Frémont <lc.fremont@gmail.com>
2020-03-09 14:53:26 +0100

    Move user interfaces into core component

---
+++ Model/User/AdminAvatarInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Monofony\Component\Core\Model\User;
+
+use App\Entity\Media\FileInterface;
+
+interface AdminAvatarInterface extends FileInterface
+{
+}
